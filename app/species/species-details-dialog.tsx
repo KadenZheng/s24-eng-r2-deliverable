2afc76456f14c014041082022c2980f185f2802c
Kaden Zheng <KadenZheng@mail.fresnostate.edu>
2024-02-02 18:50:02 -0500

    workshop 1 f

--- app/species/species-details-dialog.tsx
+++ app/species/species-details-dialog.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { Icons } from "@/components/icons";
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
@@ -16,22 +15,15 @@ type Species = Database["public"]["Tables"]["species"]["Row"];
 
 export default function SpeciesDetailsDialog({ species }: { species: Species }) {
   return (
-    <Dialog open={open} onOpenChange={setOpen}>
+    <Dialog>
       <DialogTrigger asChild>
-        <Button variant="secondary">
-          <Icons.add className="mr-3 h-5 w-5" />
-          Add Species
-        </Button>
+        <Button className="mt-3 w-full">Learn More</Button>
       </DialogTrigger>
       <DialogContent className="max-h-screen overflow-y-auto sm:max-w-[600px]">
         <DialogHeader>
           <DialogTitle>{species.scientific_name}</DialogTitle>
           {species.common_name && <DialogDescription>{species.common_name}</DialogDescription>}
-          <DialogDescription>
-            Add a new species here. Click &quot;Add Species&quot; below when you&apos;re done.
-          </DialogDescription>
         </DialogHeader>
-        {/* TODO: */}
       </DialogContent>
     </Dialog>
   );
